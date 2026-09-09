fdf1b868f23aca9624bb65ae0b31526fe65e83c1
andradesophia271-ops <andradesophia271@gmail.com>
2026-09-09 11:14:11 -0300

    Corrige sintaxe e implementa AlgoritmoQuinze

--- javaprojetos/src/AlgoritmoQuinze.java
+++ javaprojetos/src/AlgoritmoQuinze.java
@@ -1,6 +1,7 @@
-public class AlgoritmoQuinze {
+public class AlgoritmoQuinze{
+
     public static void main(String[] args) {
-        for (int i = 0; i <= 200; i += 2) {
+        for(int i = 0; i <= 200; i++) {
             System.out.println(i);
         }
     }
